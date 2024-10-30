fea6ab521b1c3f661f81cd33376264602ed8414a
PEARLKIMANI <109605254+PEARLKIMANI@users.noreply.github.com>
2024-10-30 12:58:55 +0300

    Add files via upload

---
+++ smartcontract.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Counter {
+    uint public count;
+
+    function increment() public {
+        count++;
+    }
+
+    function decrement() public {
+        require(count > 0, "Count cannot be negative");
+        count--;
+    }
+}
